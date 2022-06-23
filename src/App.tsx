029aed3fb4bbdd3474e1d03255666fdd803068df
최준경 <choejungyeong@ip-172-30-1-34.ap-northeast-2.compute.internal>
2022-06-23 16:32:16 +0900

    초기 설정

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+import Styles from './common/Styles';
+
+const App = () => {
+  return (
+    <SafeAreaView style={Styles.container}>
+      <Text>Hello Worlds!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ src/common/Styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    height: "100%",
+    fontSize: 40,
+    fontWeight: "700",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
