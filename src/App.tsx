04ab3ee480681083112a82eb160988ca0466bd51
최준경 <choejungyeong@ip-172-30-1-34.ap-northeast-2.compute.internal>
2022-06-23 17:24:55 +0900

    네비게이터 기초 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 import Styles from './common/Styles';
 
 const App = () => {
   return (
-    <SafeAreaView style={Styles.container}>
-      <Text>Hello Worlds!</Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      <SafeAreaView style={Styles.container}>
+        <Text>Hello Worlds!</Text>
+      </SafeAreaView>
+    </NavigationContainer>
   );
 };
 
